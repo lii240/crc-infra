4de9b5b164d562810000167db883cfe96e3284c0
LH <liban240@gmail.com>
2023-09-06 12:57:23 +0100

    backup terraform state and reconfigure init

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Function to perform state backup
+backup_state() {
+  terraform state pull > terraform.tfstate.backup
+}
+
+# Function to handle backend configuration change
+handle_backend_change() {
+  # Perform state backup
+  backup_state
+
+  # Reinitialize Terraform to apply new backend configuration
+  terraform init -reconfigure
+
+  # Push the state to the new backend
+  terraform state push terraform.tfstate.backup
+}
+
+# Initialize Terraform
+terraform init
+
 # Detect changes in backend configuration
 if [[ "$(terraform init -reconfigure 2>&1 | grep 'A change in the backend configuration')" ]]; then
   echo "Detected backend configuration change. Handling state migration..."
